2e04bc7e852cde01f5036e8b55010727546c2e8d
Vorapahct <iordtchap@gmail.com>
2018-03-15 00:07:05 +0200

    Added a function for PASV

--- FTPclient.py
+++ FTPclient.py
@@ -1,6 +1,34 @@
 import socket
 
 
+def passiveConnect(client_socket):
+    client_socket.send('PASV\r\n'.encode())
+    response = client_socket.recv(8192).decode()
+
+    print("This is the PASV response: " + response)
+    index_start = response.find('(')
+    index_end = response.find(')')
+    response = response[index_start + 1:index_end]  # Removes the brackets to the string from PASV
+    response = response.split(",")                  # Splits the numbers into an array
+
+    data_host = ''
+
+    for i in range(0, 4):
+        data_host = data_host + (response[i]) + '.'
+
+    data_host = data_host[:-1]
+    end_response = response[-2:]
+    data_port = (int(end_response[0]) * 256) + int(end_response[1])
+    data_port = int(data_port)
+
+    print("Data Host: %s" % data_host)
+    print('Data Port: %d' % data_port)
+
+    data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    data_socket.connect((data_host, data_port))
+    return(data_socket)
+
+
 def main():
     host = 'ftp.uconn.edu'
     command_port = 21
@@ -15,76 +43,19 @@ def main():
     client_socket.send('PASS anonymous@\r\n'.encode())
     print(client_socket.recv(8192).decode())
 
-    client_socket.send('PASV\r\n'.encode())
-    resp = client_socket.recv(8192).decode()
-
-    print("This is the response: " + resp)
-    index = resp.find('(')
-    endindex = resp.find(')')
-    resp = resp[index+1:endindex]
-    print("This is the cut content of the index: " + resp)
-    resp = resp.split(",")
-    print(resp)
-
-    data_host = ''
-
-    for i in range(0, 4):
-        data_host = data_host + (resp[i]) + '.'
-
-    data_host = data_host[:-1]
-    print('Here is the data host\n')
-    print(data_host)
-
-    endResp = resp[-2:]
-    print(endResp)
-    data_port = (int(endResp[0]) * 256) + int(endResp[1])
-    data_port = int(data_port)
-
-    print("Data Host: %s" % data_host)
-    print('Data Port: %d' % data_port)
-
-    data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    data_socket.connect((data_host, data_port))
+    data_socket = passiveConnect(client_socket)
 
     client_socket.send('LIST\r\n'.encode())
-    print("Results of 1st LIST: " + client_socket.recv(8192).decode())
+    print("Results of LIST command trigger: " + client_socket.recv(8192).decode())
     print("Result of the data socket: \n" + data_socket.recv(8192).decode())
-    print("Results of 1st LIST again: " + client_socket.recv(8192).decode())
+    print("Results of LIST command end: " + client_socket.recv(8192).decode())
 
-    client_socket.send('PASV\r\n'.encode())
-    resp = client_socket.recv(8192).decode()
-    print("This is the response: " + resp)
-    index = resp.find('(')
-    endindex = resp.find(')')
-    resp = resp[index + 1:endindex]
-    print("This is the cut content of the index: " + resp)
-    resp = resp.split(",")
-    print(resp)
-
-    data_host = ''
-
-    for i in range(0, 4):
-        data_host = data_host + (resp[i]) + '.'
-
-    data_host = data_host[:-1]
-    print('Here is the data host\n')
-    print(data_host)
-
-    endResp = resp[-2:]
-    print(endResp)
-    data_port = (int(endResp[0]) * 256) + int(endResp[1])
-    data_port = int(data_port)
-
-    print("Data Host: %s" % data_host)
-    print('Data Port: %d' % data_port)
-
-    data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    data_socket.connect((data_host, data_port))
+    data_socket = passiveConnect(client_socket)
 
     client_socket.send('RETR 48_hour/testZilla.txt\r\n'.encode())
-    print("Results of RETR: " + client_socket.recv(8192).decode())
+    print("Results of RETR command: " + client_socket.recv(8192).decode())
     file_data = data_socket.recv(8192)
-    print("Results of data: \n" + file_data.decode())
+    print("Results of data connection: \n" + file_data.decode())
 
     with open('testZilla.txt', 'wb') as File:
         File.write(file_data)
