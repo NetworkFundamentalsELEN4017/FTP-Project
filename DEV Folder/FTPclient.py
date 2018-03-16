06a0239f0f68a3515e5913a2c3084942bbf424aa
Vorapahct <1068874@students.wits.ac.za>
2018-03-16 23:37:55 +0200

    PASV & LIST between own client & server

--- DEV Folder/FTPclient.py
+++ DEV Folder/FTPclient.py
@@ -1,10 +1,10 @@
 import socket
 
 
-def passiveConnect(client_socket):
+def passiveConnect(command_socket):
 
-    client_socket.send('PASV\r\n'.encode())
-    response = client_socket.recv(8192).decode()
+    command_socket.send('PASV\r\n'.encode())
+    response = command_socket.recv(8192).decode()
     print("This is the PASV response: " + response)
     index_start = response.find('(')
     index_end = response.find(')')
@@ -17,8 +17,8 @@ def passiveConnect(client_socket):
     data_port = (int(port_response[0]) * 256) + int(port_response[1])
     data_port = int(data_port)
     # Uncomment to print the IP address and the port that the client is trying to connect to
-    # print("Data Host: %s" % data_host)
-    # print('Data Port: %d' % data_port)
+    print("Data Host: %s" % data_host)
+    print('Data Port: %d' % data_port)
     data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     data_socket.connect((data_host, data_port))
     return data_socket
@@ -30,32 +30,32 @@ def main():
     server_address = socket.gethostbyname(socket.gethostname())
     command_port = 5000
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_address, command_port))
-    #print(client_socket.recv(8192).decode())
+    command_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    command_socket.connect((server_address, command_port))
+    print("Command socket set")
+    '''
+    command_socket.send('USER group18\r\n'.encode())
+    print(command_socket.recv(8192).decode())
 
-    client_socket.send('USER group18\r\n'.encode())
-    print(client_socket.recv(8192).decode())
-
-    client_socket.send('PASS Tee3ho3d\r\n'.encode())
-    print(client_socket.recv(8192).decode())
-
+    command_socket.send('PASS Tee3ho3d\r\n'.encode())
+    print(command_socket.recv(8192).decode())
+    '''
     # Attempt to setup up passive FTP mode and connect data TCP connection
-    data_socket = passiveConnect(client_socket)
+    data_socket = passiveConnect(command_socket)
 
-    client_socket.send('LIST\r\n'.encode())
-    print(client_socket.recv(8192).decode())
-    print(client_socket.recv(8192).decode())
+    print("Asking for LIST now")
+    command_socket.send('LIST\r\n'.encode())
+    print(command_socket.recv(8192).decode())
+    print(command_socket.recv(8192).decode())
     print(data_socket.recv(8192).decode())
-    # data_socket.close()
 
     # Download Binary files
-    client_socket.send('TYPE I\r\n'.encode())
-    print("Results of Type B: " + client_socket.recv(8192).decode())
-    data_socket = passiveConnect(client_socket)
+    command_socket.send('TYPE I\r\n'.encode())
+    print("Results of Type B: " + command_socket.recv(8192).decode())
+    data_socket = passiveConnect(command_socket)
 
-    client_socket.send('RETR /files/slav.jpg\r\n'.encode())
-    print("Results of RETR command: " + client_socket.recv(8192).decode())
+    command_socket.send('RETR /files/slav.jpg\r\n'.encode())
+    print("Results of RETR command: " + command_socket.recv(8192).decode())
 
     file_data = data_socket.recv(8192)
     f = open("slav.jpg", 'wb')
@@ -64,15 +64,15 @@ def main():
         f.write(file_data)
         file_data = data_socket.recv(8192)
 
-    print("Results of RETR command again: " + client_socket.recv(8192).decode())
+    print("Results of RETR command again: " + command_socket.recv(8192).decode())
 
     # Upload binary files
-    client_socket.send('TYPE I\r\n'.encode())
-    print("Results of Type B: " + client_socket.recv(8192).decode())
-    data_socket = passiveConnect(client_socket)
+    command_socket.send('TYPE I\r\n'.encode())
+    print("Results of Type B: " + command_socket.recv(8192).decode())
+    data_socket = passiveConnect(command_socket)
 
-    client_socket.send('STOR /files/Sloth.jpg\r\n'.encode())
-    print("Results of RETR command: " + client_socket.recv(8192).decode())
+    command_socket.send('STOR /files/Sloth.jpg\r\n'.encode())
+    print("Results of RETR command: " + command_socket.recv(8192).decode())
 
     pic = open('Sloth.jpg', 'rb')
     reading = pic.read(8192)
@@ -87,37 +87,37 @@ def main():
     pic.close()
     data_socket.close()
 
-    Reply = client_socket.recv(4096).decode("UTF-8")
+    Reply = command_socket.recv(4096).decode("UTF-8")
     print('Control connection reply: \n' + str(Reply))
 
-    client_socket.send('STRU F\r\n'.encode())
-    print("Results of Stru F: " + client_socket.recv(8192).decode())
+    command_socket.send('STRU F\r\n'.encode())
+    print("Results of Stru F: " + command_socket.recv(8192).decode())
 
-    client_socket.send('STRU R\r\n'.encode())
-    print("Results of Stru R: " + client_socket.recv(8192).decode())
+    command_socket.send('STRU R\r\n'.encode())
+    print("Results of Stru R: " + command_socket.recv(8192).decode())
 
-    client_socket.send('STRU P\r\n'.encode())
-    print("Results of Stru P: " + client_socket.recv(8192).decode())
+    command_socket.send('STRU P\r\n'.encode())
+    print("Results of Stru P: " + command_socket.recv(8192).decode())
 
-    client_socket.send('MODE S\r\n'.encode())
-    print("Results of Mode S: " + client_socket.recv(8192).decode())
+    command_socket.send('MODE S\r\n'.encode())
+    print("Results of Mode S: " + command_socket.recv(8192).decode())
 
-    client_socket.send('MODE B\r\n'.encode())
-    print("Results of Mode B: " + client_socket.recv(8192).decode())
+    command_socket.send('MODE B\r\n'.encode())
+    print("Results of Mode B: " + command_socket.recv(8192).decode())
 
-    client_socket.send('MODE C\r\n'.encode())
-    print("Results of Mode C: " + client_socket.recv(8192).decode())
+    command_socket.send('MODE C\r\n'.encode())
+    print("Results of Mode C: " + command_socket.recv(8192).decode())
 
-    client_socket.send('PORT 10,30,126,164,47,56\r\n'.encode())
-    print("Results of PORT: " + client_socket.recv(8192).decode())
+    command_socket.send('PORT 10,30,126,164,47,56\r\n'.encode())
+    print("Results of PORT: " + command_socket.recv(8192).decode())
 
-    client_socket.send('NOOP\r\n'.encode())
-    print("Results of Noop: " + client_socket.recv(8192).decode())
+    command_socket.send('NOOP\r\n'.encode())
+    print("Results of Noop: " + command_socket.recv(8192).decode())
 
-    client_socket.send('QUIT\r\n'.encode())
-    print("Results of Quit: " + client_socket.recv(8192).decode())
+    command_socket.send('QUIT\r\n'.encode())
+    print("Results of Quit: " + command_socket.recv(8192).decode())
 
-    client_socket.close()
+    command_socket.close()
     data_socket.close()
 
 if __name__ == '__main__':
--- DEV Folder/FTPserver.py
+++ DEV Folder/FTPserver.py
@@ -1,5 +1,6 @@
 import socket
 
+
 def commandEstablish(host, port):
     command_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     command_socket.bind((host, port))
@@ -8,6 +9,7 @@ def commandEstablish(host, port):
 def dataEstablish(host, port):
     data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     data_socket.bind((host, port))
+    data_socket.listen(1)
     return data_socket
 
 def Main():
@@ -16,25 +18,49 @@ def Main():
     port = 5000
 
     command_socket = commandEstablish(host, port)
-    print("Server is running on port", port)
+    print("Command:",command_socket.getsockname())
     command_socket.listen(1)
 
-    c, address = command_socket.accept()
-    print("Connection from: " + str(address))
+    command_connection, command_address = command_socket.accept()
+    print("Command Address:",command_address)
+
     while True:
-        data = c.recv(1024).decode()
+
+        data = command_connection.recv(1024).decode()
         print("From connected client: " + data)
         command = data[:4]
         message = data[5:].strip()
-
+        '''
         if command == "USER":
             if message == "group18":
                 print("Hello", message)
-                c.send("200 OK".encode())
+                command_connection.send("331 Please Specify Password".encode())
 
-        if not data:
+        if command == "PASS":
+            if message == "Tee3ho3d":
+                print("Password accepted", message)
+                command_connection.send("230 Login successful".encode())
+            else:
+                command_connection.send("530 Login incorrect".encode())
+        '''
+        if command == "PASV":
+            server_address = socket.gethostbyname(socket.gethostname())
+            server_address = server_address.split(".")
+            server_address = ','.join(server_address)
+            server_address = "("+ server_address + ",30,60)"
+            command_connection.send(server_address.encode())
+
+            data_socket = dataEstablish("127.0.1.1",7740)
+
+        if command == "LIST":
+            command_connection.send("150 Listing Directory".encode())
+            command_connection.send("200 OK".encode())
+            data_connection, address = data_socket.accept()
+            data_connection.send("This would be the directory".encode())
+
+        if not command:
             break
-    c.close()
+    command_connection.close()
 
 if __name__ == '__main__':
     Main()
