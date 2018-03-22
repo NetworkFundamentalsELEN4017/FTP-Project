90b837132d686c7001bdb02213338517fb376dd5
Kavilan Nair <kavilann@gmail.com>
2018-03-22 16:40:44 +0200

    Updated to work with our client code

--- DEV Folder/FTP_Server.py
+++ DEV Folder/FTP_Server.py
@@ -11,13 +11,20 @@ class FTPServer (threading.Thread):
         self.address_ip = address_ip
         self.data_connection = None
         self.type = None
+        self.isConnectionTerminated = False
 
     def run(self):
         print("Connection from: ", str(self.address_ip))
         self.command_connection.send('220 Welcome to the FTP server\r\n'.encode())
 
         while True:
-            commands_available = ['USER', 'PASS', 'PASV', 'LIST', 'PWD', 'CWD', 'TYPE', 'SYST', 'RETR']
+            commands_available = ['USER', 'PASS', 'PASV', 'LIST', 'PWD', 'CWD', 'TYPE', 'SYST', 'RETR', 'STOR', 'NOOP',
+                                  'QUIT']
+
+            if self.isConnectionTerminated:
+                break
+
+            print('Out the loop')
 
             client_message = self.command_connection.recv(1024).decode()
             print("From connected client: " + client_message)
@@ -43,7 +50,6 @@ class FTPServer (threading.Thread):
                     port_number2 = random.randint(0, 255)
                     # port_number1 = 60
                     # port_number2 = 30
-                    data_port = (port_number1*256) + port_number2
                     server_address = socket.gethostbyname(socket.gethostname())
                     server_address = server_address.split(".")
                     server_address = ','.join(server_address)
@@ -65,7 +71,7 @@ class FTPServer (threading.Thread):
 
                     self.command_connection.send('226 List is done transferring\r\n'.encode())
                     data_sock.close()
-                    # data_connection.close()
+                    data_connection.close()
 
                 if command == 'PWD':
                     print('pwd code')
@@ -97,11 +103,10 @@ class FTPServer (threading.Thread):
                     self.command_connection.send("215 MAC \r\n".encode())
 
                 if command == 'RETR':
+                    self.command_connection.send('150 File status okay; about to open data connection.\r\n'.encode())
                     data_sock, data_address = data_connection.accept()
                     filename = argument
-
                     if self.type == 'A':
-
                         file = open(filename, 'r')
                         reading = file.read(8192)
 
@@ -112,6 +117,7 @@ class FTPServer (threading.Thread):
 
                         file.close()
                         data_sock.close()
+                        data_connection.close()
                         self.command_connection.send('226 file transfer completed \r\n'.encode())
                         # should I close the data_connection
 
@@ -126,8 +132,55 @@ class FTPServer (threading.Thread):
 
                         file.close()
                         data_sock.close()
+                        data_connection.close()
                         self.command_connection.send('226 file transfer completed \r\n'.encode())
                         # should I close the data_connection
+
+                if command == 'STOR':
+                    self.command_connection.send('150 File status okay; about to open data connection.\r\n'.encode())
+                    data_sock, data_address = data_connection.accept()
+                    filename = argument
+                    if self.type == 'A':
+                        file = open(filename, 'w')
+                        file_data = data_sock.recv(8192).decode()
+
+                        while file_data:
+                            print('writing file')
+                            file.write(file_data)
+                            file_data = data_sock.recv(8192).decode()
+
+                        file.close()
+                        data_sock.close()
+                        data_connection.close()
+                        self.command_connection.send('226 file transfer completed \r\n'.encode())
+                        # should I close the data_connection
+
+                    elif self.type == 'I':
+                        file = open(filename, 'wb')
+                        file_data = data_sock.recv(8192)
+
+                        while file_data:
+                            print('writing file')
+                            file.write(file_data)
+                            file_data = data_sock.recv(8192)
+
+                        file.close()
+                        data_sock.close()
+                        data_connection.close()
+
+                        self.command_connection.send('226 file transfer completed \r\n'.encode())
+                        # should I close the data_connection
+
+                        file.close()
+                        data_sock.close()
+
+                if command == 'NOOP':
+                    self.command_connection.send('200 NOOP OK \r\n'.encode())
+
+                if command == 'QUIT':
+                    self.command_connection.send('221 Goodbye\r\n'.encode())
+                    self.command_connection.close()
+                    self.isConnectionTerminated = True
 
             elif command not in commands_available:
                 self.command_connection.send("502 Command not implemented \r\n".encode())
@@ -142,18 +195,18 @@ class FTPServer (threading.Thread):
 def main():
     # Local Machine IP
     host = socket.gethostbyname(socket.gethostname())
-    port = 5004
+    port = 5001
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     print('FTP Server initialized at ' + host)
+    print("Awaiting a connection from a client")
 
     while True:
         server_socket.listen(1)
         connection_socket, address_ip = server_socket.accept()
         thread = FTPServer(connection_socket, address_ip)
         thread.start()
-
 
 if __name__ == '__main__':
     main()
