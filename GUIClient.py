5eac88d4981b0573c1b1ce0aea0c21b605dda6dc
Vorapahct <iordtchap@gmail.com>
2018-03-29 13:03:15 +0200

    Upload not working

--- GUIClient.py
+++ GUIClient.py
@@ -12,12 +12,16 @@ class Login(QDialog):
         loadUi('Login.ui', self)
         self.setWindowTitle('FTP Client')
         self.btnLogin.clicked.connect(self.onLoginButtonClicked)
+        self.btnDirectory.clicked.connect(self.directory_change)
         self.treeViewUp.hide()
-        self.treeViewDown.hide()
+        self.pteDownload.hide()
         self.btnUpload.hide()
+        self.btnDirectory.hide()
         self.btnDownload.hide()
+        self.edtDirectory.hide()
+        self.edtUpload.hide()
         self.treeViewUp.clicked.connect(self.onClicked)
-        self.treeViewDown.clicked.connect(self.onClicked)
+        self.btnUpload.clicked.connect(self.upload_file)
 
     def onLoginButtonClicked(self):
         self.frmLogin.hide()
@@ -28,30 +32,30 @@ class Login(QDialog):
         password = self.edtPass.text()
         command_port = self.edtPort.text()
 
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((host, int(command_port)))
-        connection_owner = client_socket.recv(8192).decode()
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((host, int(command_port)))
+        connection_owner = self.client_socket.recv(8192).decode()
         self.pteConsole.appendPlainText("Connected to: " + connection_owner)
 
         cmd_user = 'USER ' + name + '\r\n'
-        self.send_cmd(client_socket, cmd_user)
+        self.send_cmd(self.client_socket, cmd_user)
 
         cmd_pass = 'PASS ' + password + '\r\n'
-        self.send_cmd(client_socket, cmd_pass)
+        self.send_cmd(self.client_socket, cmd_pass)
 
         cmd_pasv = 'PASV\r\n'
-        response = self.send_cmd(client_socket, cmd_pasv)
+        response = self.send_cmd(self.client_socket, cmd_pasv)
         data_socket = self.setup_data(response)
 
         cmd_list = 'LIST\r\n'
-        self.send_cmd(client_socket, cmd_list)
+        self.send_cmd(self.client_socket, cmd_list)
         response = data_socket.recv(8192).decode()
-        #print(response)
+        self.pteDownload.appendPlainText(response)
 
-        directory = os.listdir()
-        #print(directory)
+        data_socket.close()
+
         directory = os.getcwd()
-        #print(directory)
+        print(directory)
         self.model.setRootPath(str(directory))
 
         self.treeViewUp.setModel(self.model)
@@ -62,13 +66,53 @@ class Login(QDialog):
         self.treeViewUp.show()
         self.btnUpload.show()
         self.btnDownload.show()
-        self.treeViewDown.show()
+        self.pteDownload.show()
+        self.btnDirectory.show()
+        self.edtDirectory.show()
+        self.edtUpload.show()
 
+    def directory_change(self):
+        cmd_cwd = 'CWD ' + self.edtDirectory.text() + '\r\n'
+        self.send_cmd(self.client_socket, cmd_cwd)
+
+        #cmd_pasv = 'PASV\r\n'
+        #response = self.send_cmd(self.client_socket, cmd_pasv)
+        #data_socket = self.setup_data(response)
+        #cmd_list = 'LIST\r\n'
+        #self.send_cmd(self.client_socket, cmd_list)
+        #response = data_socket.recv(8192).decode()
+        #self.pteDownload.appendPlainText(response)
+
+    def upload_file(self):
+        cmd_type = 'TYPE I\r\n'
+        self.send_cmd(self.client_socket, cmd_type)
+
+        cmd_pasv = 'PASV\r\n'
+        response = self.send_cmd(self.client_socket, cmd_pasv)
+        data_socket = self.setup_data(response)
+
+        #cmd_stor = 'STOR ' + self.edtUpload.text() + '\r\n'
+        cmd_stor = 'STOR ' + 'Sloth.jpg' + '\r\n'
+        self.send_cmd(self.client_socket, cmd_stor)
+
+        pic = open('Sloth.jpg', 'rb')
+        reading = pic.read(8192)
+
+        while reading:
+            print('reading file')
+            data_socket.send(reading)
+            reading = pic.read(8192)
+
+        data_socket.close()
 
     def onClicked(self, index):
         path = self.sender().model().filePath(index)
-        self.lblOutput.setText(path)
-        print(path)
+        self.edtUpload.setText(path)
+        #self.pteConsole.appendPlainText("Selected File Path: " + path)
+        #cmd_cwd = self.edtDirectory + '\r\n'
+        #self.send_cmd(self.client_socket,cmd_cwd)
+
+
 
     def send_cmd(self, client_socket, command):
         client_socket.send(command.encode())
