d79f8d0deaeb4a1c6917ce22058ac2d5bd9d8c8f
Vorapahct <iordtchap@gmail.com>
2018-03-30 13:39:03 +0200

    Download + Upload (not perfect)

--- GUIClient.py
+++ GUIClient.py
@@ -20,8 +20,16 @@ class Login(QDialog):
         self.btnDownload.hide()
         self.edtDirectory.hide()
         self.edtUpload.hide()
+        self.lblServer.hide()
+        self.lblClient.hide()
+        self.edtDownload.hide()
+        self.lblDownload.hide()
+        self.lblUpload.hide()
+        self.btnRefresh.hide()
         self.treeViewUp.clicked.connect(self.onClicked)
         self.btnUpload.clicked.connect(self.upload_file)
+        self.btnDownload.clicked.connect(self.download_file)
+        self.btnRefresh.clicked.connect(self.refresh_directory)
 
     def onLoginButtonClicked(self):
         self.frmLogin.hide()
@@ -69,45 +77,97 @@ class Login(QDialog):
         self.pteDownload.show()
         self.btnDirectory.show()
         self.edtDirectory.show()
+        self.lblServer.show()
+        self.lblClient.show()
         self.edtUpload.show()
+        self.edtDownload.show()
+        self.lblDownload.show()
+        self.lblUpload.show()
+        self.btnRefresh.show()
 
     def directory_change(self):
         cmd_cwd = 'CWD ' + self.edtDirectory.text() + '\r\n'
         self.send_cmd(self.client_socket, cmd_cwd)
 
-        #cmd_pasv = 'PASV\r\n'
-        #response = self.send_cmd(self.client_socket, cmd_pasv)
-        #data_socket = self.setup_data(response)
-        #cmd_list = 'LIST\r\n'
-        #self.send_cmd(self.client_socket, cmd_list)
-        #response = data_socket.recv(8192).decode()
-        #self.pteDownload.appendPlainText(response)
-
     def upload_file(self):
-        cmd_type = 'TYPE I\r\n'
-        self.send_cmd(self.client_socket, cmd_type)
+        user_file_name = self.edtUpload.text()
+        print(user_file_name)
+        if user_file_name:
+            filetype = user_file_name.split('.')[-1]
+            print(filetype)
+            if filetype == 'txt':
+                cmd_type = 'TYPE A\r\n'
+            else:
+                cmd_type = 'TYPE I\r\n'
+            self.send_cmd(self.client_socket, cmd_type)
 
         cmd_pasv = 'PASV\r\n'
         response = self.send_cmd(self.client_socket, cmd_pasv)
         data_socket = self.setup_data(response)
 
-        #cmd_stor = 'STOR ' + self.edtUpload.text() + '\r\n'
-        cmd_stor = 'STOR ' + 'Sloth.jpg' + '\r\n'
+        cmd_stor = 'STOR ' + user_file_name + '\r\n'
+        #cmd_stor = 'STOR ' + 'Sloth1.jpg' + '\r\n'
         self.send_cmd(self.client_socket, cmd_stor)
 
-        pic = open('Sloth.jpg', 'rb')
+        pic = open(user_file_name, 'rb')
         reading = pic.read(8192)
 
         while reading:
             print('reading file')
             data_socket.send(reading)
             reading = pic.read(8192)
-
         data_socket.close()
+        self.pteConsole.appendPlainText("Return message: " + self.client_socket.recv(8192).decode())
+
+    def download_file(self):
+        down_folder_name = "Server_Downloads"
+        if not os.path.exists(down_folder_name):
+            os.makedirs(down_folder_name)
+
+        user_file_name = self.edtDownload.text()
+        if user_file_name:
+            filetype = user_file_name.split('.')[-1]
+            if filetype == 'txt':
+                cmd_type = 'TYPE A\r\n'
+            else:
+                cmd_type = 'TYPE I\r\n'
+            self.send_cmd(self.client_socket, cmd_type)
+
+            cmd_pasv = 'PASV\r\n'
+            response = self.send_cmd(self.client_socket, cmd_pasv)
+            data_socket = self.setup_data(response)
+
+            cmd_stor = 'RETR ' + user_file_name + '\r\n'
+            #cmd_stor = 'RETR ' + 'Sloth1.jpg' + '\r\n'
+            self.send_cmd(self.client_socket, cmd_stor)
+
+            file_data = data_socket.recv(8192)
+            f = open(down_folder_name + "/Sloth1.jpg", 'wb')
+
+            while file_data:
+                f.write(file_data)
+                file_data = data_socket.recv(8192)
+
+            self.pteConsole.appendPlainText(self.client_socket.recv(8192).decode())
+        else:
+            self.pteConsole.appendPlainText("Please select a file to download.")
+
+    def refresh_directory(self):
+
+        cmd_pasv = 'PASV\r\n'
+        response = self.send_cmd(self.client_socket, cmd_pasv)
+        data_socket = self.setup_data(response)
+        cmd_list = 'LIST\r\n'
+        self.send_cmd(self.client_socket, cmd_list)
+        response = data_socket.recv(8192).decode()
+        self.pteDownload.setPlainText(response)
+        data_socket.close()
+
 
     def onClicked(self, index):
         path = self.sender().model().filePath(index)
-        self.edtUpload.setText(path)
+        filename = path.split('/')[-1]
+        self.edtUpload.setText(filename)
         #self.pteConsole.appendPlainText("Selected File Path: " + path)
         #cmd_cwd = self.edtDirectory + '\r\n'
         #self.send_cmd(self.client_socket,cmd_cwd)
